41725bd46dc4833ac73fbbbce502ea920b9cd5b6
Glenn Block <glenn.block@gmail.com>
2013-03-03 14:07:00 -0800

    adding missing empty test

---
+++ test/Scriptcs.Tests/PackageAssemblyResolverTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Moq;
+
+namespace Scriptcs.Tests
+{
+    public class PackageAssemblyResolverTests
+    {
+        private Mock<IFileSystem> fileSystem = new Mock<IFileSystem>();
+
+        /*
+        public PackageAssemblyResolverTests()
+        {
+            var results = new List<string>()
+                {
+                    @"packages\package1\lib\net40\net40.dll",
+                    @"c:\packages\package1\lib\net20\net20.dll"
+                };
+
+            fileSystem.Setup(f=>f.EnumerateFiles(It.IsAny<string>(), It.is))
+        }
+         */
+
+        [Fact]
+        public void ShouldReturnNet40PackageAssemblyNamesWhenGetAssemblyNamesIsCalled()
+        {
+            
+        }
+
+    }
+}
