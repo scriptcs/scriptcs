3be22e500eb731ed5bb70ff2a691a6e776a07b1f
Justin Rusbatch <jrusbatch@gmail.com>
2013-03-03 20:50:10 -0500

    ScriptEngine logic should be contained in the Scriptcs.Core project

--- src/Scriptcs.Core/IRecipeManager.cs
+++ src/Scriptcs.Core/IRecipeManager.cs
@@ -1,9 +1,5 @@
-﻿using System;
+﻿using Scriptcs.Contracts;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Scriptcs.Contracts;
 
 namespace Scriptcs
 {
