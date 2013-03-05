f0226bf720ff7b5831b5f4b2beb619a1d5b67066
dpen2000 <davidpendray@gmail.com>
2013-03-05 22:30:11 +0000

    Tests of existing functionality of PackageAssemblyResolver

--- test/Scriptcs.Core.Tests/PackageAssemblyResolverTests.cs
+++ test/Scriptcs.Core.Tests/PackageAssemblyResolverTests.cs
@@ -4,31 +4,42 @@ using System.Linq;
 using System.Text;
 using Xunit;
 using Moq;
+using Should;
 
 namespace Scriptcs.Tests
 {
     public class PackageAssemblyResolverTests
     {
         private Mock<IFileSystem> fileSystem = new Mock<IFileSystem>();
+        private readonly string Package1Net40AssemblyName = @"package1\lib\net40\net40.dll";
+        private readonly string Package2Net35AssemblyName = @"package2\lib\net35\net35.dll";
+        private IEnumerable<string> assemblyNames;
 
-        /*
         public PackageAssemblyResolverTests()
         {
             var results = new List<string>()
                 {
-                    @"packages\package1\lib\net40\net40.dll",
-                    @"c:\packages\package1\lib\net20\net20.dll"
+                    Package1Net40AssemblyName,
+                    Package2Net35AssemblyName,
                 };
 
-            fileSystem.Setup(f=>f.EnumerateFiles(It.IsAny<string>(), It.is))
+            fileSystem.Setup(f => f.EnumerateFiles(It.IsAny<string>(), It.IsAny<string>())).Returns(() => results);
+            fileSystem.Setup(f => f.DirectoryExists(It.IsAny<string>())).Returns(true);
+            var packageAssemblyResolver = new PackageAssemblyResolver(fileSystem.Object);
+            assemblyNames = packageAssemblyResolver.GetAssemblyNames();
         }
-         */
+
 
         [Fact]
-        public void ShouldReturnNet40PackageAssemblyNamesWhenGetAssemblyNamesIsCalled()
+        public void ShouldReturnNet40PackageAssemblyNamesWhenGetAssemblyNamesIsCalled_AndNoneHigherAreAvailable()
         {
-            
+            assemblyNames.ShouldContain(Package1Net40AssemblyName);
         }
 
+        [Fact]
+        public void ShouldReturnNet35PackageAssemblyNamesWhenGetAssemblyNamesIsCalled_WhenNoHigherIsAvailable()
+        {
+            assemblyNames.ShouldContain(Package2Net35AssemblyName);
+        }
     }
 }
