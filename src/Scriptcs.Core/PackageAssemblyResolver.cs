66eafcaf33492c3205acef818b27a787e3f52069
dpen2000 <davidpendray@gmail.com>
2013-03-05 22:31:26 +0000

    Fixes #10 - Fix busted algorithm for resolving dlls within nuget pkgs - using Nuget.Core

--- src/Scriptcs.Core/PackageAssemblyResolver.cs
+++ src/Scriptcs.Core/PackageAssemblyResolver.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using NuGet;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.IO;
 using System.Linq;
+using System.Runtime.Versioning;
 
 namespace Scriptcs
 {
@@ -25,20 +27,46 @@ namespace Scriptcs
 
             var folders = new List<string>();
             var files = new List<string>();
-            foreach (var file in _fileSystem.EnumerateFiles(packageDir, @"*.dll"))
+            foreach (var packageFileGroup in _fileSystem.EnumerateFiles(packageDir, @"*.dll")
+                .Select(p => new NugetAssemblyPath(p, packageDir)).GroupBy(f => f.PackageName))
             {
-                if (file.IndexOf(@"\net35") > -1 || file.IndexOf(@"\net40") > -1)
-                {
-                    var path = Path.GetDirectoryName(file);
-                    files.Add(file);
-                    if (!folders.Contains(path))
-                    {
-                        folders.Add(path);
-                        Console.WriteLine("Found package reference: " + path);
-                    }
-                }
+                IEnumerable<NugetAssemblyPath> compatibleItems;
+                VersionUtility.TryGetCompatibleItems(VersionUtility.DefaultTargetFramework, packageFileGroup, out compatibleItems);
+                files.AddRange(compatibleItems.Select(x => x.Path));
+                var path = Path.GetDirectoryName(compatibleItems.First().Path);
+                Console.WriteLine("Found package reference: " + path);
             }
             return files;
+        }
+
+        public class NugetAssemblyPath : IFrameworkTargetable
+        {
+            public NugetAssemblyPath(string path, string packageDir)
+            {
+                Path = path;
+                PathWithoutPackageDir = path.Replace(packageDir + @"\", "");
+            }
+            public string Path { get; set; }
+
+            public string PackageName
+            {
+                get
+                {
+                    return PathWithoutPackageDir.Split(new[] { System.IO.Path.DirectorySeparatorChar }, StringSplitOptions.RemoveEmptyEntries).First();
+                }
+            }
+
+            public IEnumerable<FrameworkName> SupportedFrameworks
+            {
+                get 
+                {
+                    var pathWithoutPackageName = PathWithoutPackageDir.Remove(0, PackageName.Length + 1);
+                    string effectivePath;
+                    return new[] { VersionUtility.ParseFrameworkNameFromFilePath(pathWithoutPackageName, out effectivePath) };
+                }
+            }
+
+            public string PathWithoutPackageDir { get; set; }
         }
     }
 }
--- test/Scriptcs.Core.Tests/PackageAssemblyResolverTests.cs
+++ test/Scriptcs.Core.Tests/PackageAssemblyResolverTests.cs
@@ -11,8 +11,10 @@ namespace Scriptcs.Tests
     public class PackageAssemblyResolverTests
     {
         private Mock<IFileSystem> fileSystem = new Mock<IFileSystem>();
-        private readonly string Package1Net40AssemblyName = @"package1\lib\net40\net40.dll";
-        private readonly string Package2Net35AssemblyName = @"package2\lib\net35\net35.dll";
+        private readonly string Package1Net40AssemblyName = @"C:\someapp\packages\package1\lib\net40\net40.dll";
+        private readonly string Package2Net35AssemblyName = @"C:\someapp\packages\package2\lib\net35\net35.dll";
+        private readonly string Package1Net35AssemblyName = @"C:\someapp\packages\package1\lib\net35\net35.dll";
+        private readonly string Package3Net20AssemblyName = @"C:\someapp\packages\package3\lib\net20\net20.dll";
         private IEnumerable<string> assemblyNames;
 
         public PackageAssemblyResolverTests()
@@ -21,10 +23,13 @@ namespace Scriptcs.Tests
                 {
                     Package1Net40AssemblyName,
                     Package2Net35AssemblyName,
+                    Package1Net35AssemblyName,
+                    Package3Net20AssemblyName
                 };
 
             fileSystem.Setup(f => f.EnumerateFiles(It.IsAny<string>(), It.IsAny<string>())).Returns(() => results);
             fileSystem.Setup(f => f.DirectoryExists(It.IsAny<string>())).Returns(true);
+            fileSystem.Setup(f => f.CurrentDirectory).Returns(@"C:\someapp");
             var packageAssemblyResolver = new PackageAssemblyResolver(fileSystem.Object);
             assemblyNames = packageAssemblyResolver.GetAssemblyNames();
         }
@@ -40,6 +45,18 @@ namespace Scriptcs.Tests
         public void ShouldReturnNet35PackageAssemblyNamesWhenGetAssemblyNamesIsCalled_WhenNoHigherIsAvailable()
         {
             assemblyNames.ShouldContain(Package2Net35AssemblyName);
+        }
+        
+        [Fact]
+        public void ShouldReturnNet20PackageAssemblyNamesWhenGetAssemblyNamesIsCalled_WhenNoHigherIsAvailable()
+        {
+            assemblyNames.ShouldContain(Package3Net20AssemblyName);
+        }
+
+        [Fact]
+        public void ShouldNotReturnNet35PackageAssemblyNamesWhenGetAssemblyNamesIsCalled_WhenNet40AssemblyNameAvailable()
+        {
+            assemblyNames.ShouldNotContain(Package1Net35AssemblyName);
         }
     }
 }
