5a000e89065697a9c7283a7d606a002484d5308f
Jonas Folleso <jonas@follesoe.no>
2013-03-06 19:59:53 +0100

    Fixed bug where current directory was beeing prepended to script path even if the script path is absolute. Fixes #60.

--- src/ScriptCs.Core/ScriptExecutor.cs
+++ src/ScriptCs.Core/ScriptExecutor.cs
@@ -1,4 +1,5 @@
-﻿using Roslyn.Scripting.CSharp;
+﻿using System;
+using Roslyn.Scripting.CSharp;
 using ScriptCs.Contracts;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
@@ -23,7 +24,7 @@ namespace ScriptCs
             var engine = new ScriptEngine();
             engine.AddReference("System");
             engine.AddReference("System.Core");
-            var bin = _fileSystem.CurrentDirectory + @"\bin";
+            var bin = Path.Combine(_fileSystem.CurrentDirectory, "bin");
             engine.BaseDirectory = bin;
 
             if (!_fileSystem.DirectoryExists(bin))
@@ -31,7 +32,7 @@ namespace ScriptCs
 
             foreach (var file in paths)
             {
-                var destFile = bin + @"\" + Path.GetFileName(file);
+                var destFile = Path.Combine(bin, Path.GetFileName(file));
                 var sourceFileLastWriteTime = _fileSystem.GetLastWriteTime(file);
                 var destFileLastWriteTime = _fileSystem.GetLastWriteTime(destFile);
                 if (sourceFileLastWriteTime != destFileLastWriteTime)
@@ -41,7 +42,7 @@ namespace ScriptCs
             }
 
             var session = engine.CreateSession();
-            var path = _fileSystem.CurrentDirectory + @"\" + script;
+            var path = Path.IsPathRooted(script) ? script : Path.Combine(_fileSystem.CurrentDirectory, script);
             var csx = _filePreProcessor.ProcessFile(path);
             session.Execute(csx);
         }
