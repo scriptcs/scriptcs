e0acd20bc5e028310c53d915968137b53a6520bd
Nick Berardi <nick@managedfusion.com>
2013-03-11 23:26:46 -0400

    added [InheritedExport] back onto IScriptEngine

--- src/ScriptCs.Core/IScriptEngine.cs
+++ src/ScriptCs.Core/IScriptEngine.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
 using ScriptCs.Contracts;
 
 namespace ScriptCs
 {
+	[InheritedExport]
 	public interface IScriptEngine
 	{
 		string BaseDirectory { get; set; }
