204dddde5c4bde824f803bc236ed2268ada309b8
Dale Ragan <dale@ragan.io>
2013-03-15 10:45:20 -0400

    Creating projects for the Mono ScriptCs Engine.

---
+++ src/ScriptCs.Engine.Mono/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ScriptCs.Engine.Mono")]
+[assembly: AssemblyDescription("ScriptCs.Engine.Mono provides a Mono.CSharp-based script engine for scriptcs.")]
+
+[assembly: Guid("63280571-3e9d-4417-aca9-ca56a40cf7c8")]
