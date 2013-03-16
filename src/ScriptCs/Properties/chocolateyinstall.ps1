b7ef531ff7430082b7e906d3caa4ed2ce46b094e
Justin Rusbatch <jrusbatch@gmail.com>
2013-03-16 18:25:43 -0400

    chocolateyinstall.ps1 to make it less verbose

--- src/ScriptCs/Properties/chocolateyinstall.ps1
+++ src/ScriptCs/Properties/chocolateyinstall.ps1
@@ -1,19 +1,28 @@
 ﻿try { 
     $tools = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-    $nugetExe = "$env:ChocolateyInstall\ChocolateyInstall\nuget"
-    $binPath="$env:appdata\scriptcs"
+    $nuget = "$env:ChocolateyInstall\ChocolateyInstall\nuget"
+    $binPath = "$env:APPDATA\scriptcs"
+    $nugetPath = "$tools\nugets"
 
-    if(!(Test-Path $binpath)){mkdir $binPath}
-    Copy-Item "$tools\scriptcs\*" $binPath -force
-    mkdir "$tools\nugets"
-    "nuget.core","Roslyn.Compilers.Common","Roslyn.Compilers.CSharp","PowerArgs" |
-        % {.$nugetexe install $_ -o "$tools\nugets"}
-    Get-childItem "$tools\nugets" -filter *.dll -recurse | % {Copy-Item $_.FullName $binPath }
-    New-Item "$tools\scriptcs\scriptcs.exe.ignore" -type file -force
-    Remove-Item $tools\..\lib -recurse -force
+    New-Item $binPath -ItemType Directory -Force | Out-Null
+
+    Copy-Item "$tools\scriptcs\*" $binPath -Force
+
+    Write-Host "Retrieving NuGet dependencies..." -ForegroundColor DarkYellow
+
+    "NuGet.Core","Autofac.Mef","Roslyn.Compilers.CSharp","PowerArgs" | %{ .$nuget install $_ -o $nugetPath -nocache } | Out-Null
+
+    Get-ChildItem $nugetPath -Filter "*.dll" -Recurse | %{ Copy-Item $_.FullName $binPath -Force }
+    Remove-Item $nugetPath -Recurse -Force
+    New-Item "$tools\scriptcs\scriptcs.exe.ignore" -ItemType File -Force | Out-Null
+    
+    if (Test-Path "$tools\..\lib") {
+        Remove-Item "$tools\..\lib" -Recurse -Force
+    }
+
     Install-ChocolateyPath $binPath
-    write-host "scriptcs.exe locted in $binpath is now in your path. " -foregroundcolor darkyellow
-    write-host "You may need to open a new console for the new path to take effect. Happy scripting!" -foregroundcolor darkyellow
+    Write-Host "scriptcs.exe has been installed to $binpath and has been added to your path." -ForegroundColor DarkYellow
+    Write-Host "You may need to open a new console for the new path to take effect. Happy scripting!" -ForegroundColor DarkYellow
     Write-ChocolateySuccess 'scriptcs'
 } catch {
     Write-ChocolateyFailure 'scriptcs' "$($_.Exception.Message)"
