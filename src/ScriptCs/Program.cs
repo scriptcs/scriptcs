fc93e538ee734873cdf9a5a128b5752b42bd40fd
Denis Mentey <denis@goortom.com>
2013-04-01 16:51:33 +0300

    #161 Passing an invalid command-line argument will show arguments help.

--- src/ScriptCs/Program.cs
+++ src/ScriptCs/Program.cs
@@ -6,9 +6,20 @@ namespace ScriptCs
 {
     internal class Program
     {
-        private static int Main(string[] args)
+        private static int Main(string[] args) 
         {
-            var commandArgs = Args.Parse<ScriptCsArgs>(args);
+            ScriptCsArgs commandArgs;
+
+            try
+            {
+                commandArgs = Args.Parse<ScriptCsArgs>(args);
+            }
+            catch (ArgException ex) 
+            {
+                commandArgs = new ScriptCsArgs();
+
+                Console.WriteLine(ex.Message);
+            }
 
             var debug = commandArgs.DebugFlag;
             var compositionRoot = new CompositionRoot(debug);
