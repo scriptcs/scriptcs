fe0fb81496adab0061b8af4f94e0dfcd6d4c4bb7
ianbattersby <ian.battersby@gmail.com>
2013-04-08 18:15:26 +0100

    Added custom autofac mef sevice mapper (side-step AutoFac bug).

--- src/ScriptCs/CompositionRoot.cs
+++ src/ScriptCs/CompositionRoot.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
 using System.IO;
+using System.Reflection;
 using Autofac;
+using Autofac.Core;
 using Autofac.Integration.Mef;
 using ScriptCs.Engine.Roslyn;
 using ScriptCs.Package;
@@ -9,8 +13,13 @@ using ScriptCs.Package.InstallationProvider;
 
 namespace ScriptCs
 {
+    using System.Linq;
+
     public class CompositionRoot
     {
+        private static readonly Type _contractNameServices = typeof(ExportAttribute).Assembly.GetType("System.ComponentModel.Composition.ContractNameServices", true);
+        private static readonly PropertyInfo _typeIdentityCache = _contractNameServices.GetProperty("TypeIdentityCache", BindingFlags.GetProperty | BindingFlags.Static | BindingFlags.NonPublic);
+
         private readonly bool _debug;
         private IContainer _container;
         private ScriptServiceRoot _scriptServiceRoot;
@@ -49,19 +58,36 @@ namespace ScriptCs
             }
 
             builder.RegisterType<ScriptServiceRoot>().As<ScriptServiceRoot>();
-            var scriptPath = Path.Combine(Environment.CurrentDirectory, "bin") ;
+            var scriptPath = Path.Combine(Environment.CurrentDirectory, "bin");
             if (Directory.Exists(scriptPath))
             {
-                var catalog = new DirectoryCatalog(scriptPath, "ScriptCs*.*");
-                builder.RegisterComposablePartCatalog(catalog);
+                var catalog = new DirectoryCatalog(scriptPath);
+                builder.RegisterComposablePartCatalog(
+                    catalog,
+                    ed =>
+                        {
+                            var ct = FindType(ed.ContractName);
+                            if (ct != null) return new[] { new TypedService(ct) };
+
+                            var et = FindType((string)ed.Metadata[CompositionConstants.ExportTypeIdentityMetadataName]);
+                            if (et != null) return new[] { new KeyedService(ed.ContractName, et) };
+
+                            return new Service[0];
+                        });
             }
             _container = builder.Build();
-            _scriptServiceRoot = _container.Resolve<ScriptServiceRoot>();            
+            _scriptServiceRoot = _container.Resolve<ScriptServiceRoot>();
         }
 
         public ScriptServiceRoot GetServiceRoot()
         {
             return _scriptServiceRoot;
+        }
+
+        static Type FindType(string exportTypeIdentity)
+        {
+            var cache = (Dictionary<Type, string>)_typeIdentityCache.GetValue(null, null);
+            return cache.FirstOrDefault(kvp => kvp.Value == exportTypeIdentity).Key;
         }
     }
 }
