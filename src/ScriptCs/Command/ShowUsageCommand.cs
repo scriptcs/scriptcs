77b9a2d15d38a1033da1fd54e730ba4f12887123
Kristian Hellang <khellang@hotmail.com>
2013-05-10 19:23:20 +0200

    Disabled type and position in usage. Merge HelpCommand and InvalidCommand into ShowUsageCommand

---
+++ src/ScriptCs/Command/ShowUsageCommand.cs
@@ -0,0 +1,33 @@
+﻿using Common.Logging;
+using PowerArgs;
+
+namespace ScriptCs.Command
+{
+    internal class ShowUsageCommand : IHelpCommand, IInvalidCommand
+    {
+        private readonly ILog _logger;
+
+        private readonly bool _isValid;
+
+        public ShowUsageCommand(ILog logger, bool isValid)
+        {
+            _logger = logger;
+            _isValid = isValid;
+        }
+
+        public CommandResult Execute()
+        {
+            var options = new ArgUsageOptions { ShowPosition = false, ShowType = false };
+            var usage = ArgUsage.GetUsage<ScriptCsArgs>(options: options);
+
+            if (_isValid)
+            {
+                _logger.Info(usage);
+                return CommandResult.Success;
+            }
+
+            _logger.Error(usage);
+            return CommandResult.Error;
+        }
+    }
+}
