a2523023cbfa9bc3197981a1b7c68c7b30f433e3
Michael Wade <spikewade+github@gmail.com>
2013-06-12 12:43:17 +0100

    Refs #321 scriptcs -install ScriptCs.AzureMobileServices
    * `GetWorkingDirectory` returns the current directory if the path is
    null/whitespace.
    * Fixed assumption that the directory/path would exist.
    * Added additional unit tests.

--- test/ScriptCs.Core.Tests/FileSystemTests.cs
+++ test/ScriptCs.Core.Tests/FileSystemTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Should;
 using Xunit;
@@ -9,18 +10,18 @@ namespace ScriptCs.Tests
     {
         public class GetWorkingDirectoryMethod
         {
+            private readonly FileSystem _fileSystem = new FileSystem();
+
             [Fact]
             public void ShouldProperlyConstructWorkingDirectoryIfScriptIsRunFromRelativePath()
             {
-                const string pathToMyScriptFolder = @"..\my_script\";
+                const string pathToMyScriptFolder = @"..\my_script";
 
                 try
                 {
                     Directory.CreateDirectory(pathToMyScriptFolder);
 
-                    var fileSystem = new FileSystem();
-
-                    fileSystem.GetWorkingDirectory(pathToMyScriptFolder)
+                    _fileSystem.GetWorkingDirectory(pathToMyScriptFolder)
                               .ShouldEqual(Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, pathToMyScriptFolder)));
                 }
                 finally
@@ -28,6 +29,55 @@ namespace ScriptCs.Tests
                     if (Directory.Exists(pathToMyScriptFolder))
                         Directory.Delete(pathToMyScriptFolder);
                 }
+            }
+
+            [Fact]
+            public void ShouldReturnWorkingDirectoryIfPathIsInvalid()
+            {
+                var invalidPaths = new List<string> {"", " ", null};
+
+                foreach (var invalidPath in invalidPaths)
+                {
+                   _fileSystem.GetWorkingDirectory(invalidPath).ShouldEqual(_fileSystem.CurrentDirectory);
+                }
+            }
+
+            [Fact]
+            public void ReturnsCorrectWorkingDirectory()
+            {
+                string workingDir = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @".\working_dir\"));
+                string existingDirectoryPath = Path.GetFullPath(Path.Combine(workingDir, @".\existing_dir\"));
+                string existingFilePath = Path.GetFullPath(Path.Combine(workingDir, @".\existing_file.txt"));
+
+                try
+                {
+                    Directory.CreateDirectory(workingDir);
+                    Directory.CreateDirectory(existingDirectoryPath);
+                    File.Create(existingFilePath).Dispose();
+
+                    _fileSystem.GetWorkingDirectory(existingDirectoryPath).ShouldEqual(existingDirectoryPath);
+                    _fileSystem.GetWorkingDirectory(existingFilePath).ShouldEqual(
+                        Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @".\working_dir")));
+                }
+                finally
+                {
+                    if (Directory.Exists(existingDirectoryPath))
+                        Directory.Delete(existingDirectoryPath);
+
+                    if (File.Exists(existingFilePath))
+                        File.Delete(existingFilePath);
+
+                    if (Directory.Exists(workingDir))
+                        Directory.Delete(workingDir);
+                }
+            }
+
+            [Fact]
+            public void ReturnsCorrectWorkingDirectoryIfPathDoesNotExist()
+            {
+                const string nonExistantFilePath = @"C:\working_dir\i_dont_exist.txt";
+
+                _fileSystem.GetWorkingDirectory(nonExistantFilePath).ShouldEqual(@"C:\working_dir");
             }
         }
     }
