a211e75da65b0fbffed3133f9f2439eec99a7676
Kristian Hellang <kristian@hellang.com>
2013-08-19 13:22:26 +0200

    Moved line processors to Contracts to be able to extend current implementation

--- src/ScriptCs.Contracts/ReferenceLineProcessor.cs
+++ src/ScriptCs.Contracts/ReferenceLineProcessor.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.IO;
-using ScriptCs.Contracts;
 
-namespace ScriptCs
+namespace ScriptCs.Contracts
 {
-    public interface IReferenceLineProcessor : ILineProcessor
-    {
-    }
-
     public class ReferenceLineProcessor : DirectiveLineProcessor, IReferenceLineProcessor
     {
         private readonly IFileSystem _fileSystem;
