e6b2a56a0d7c86952c8195f91e32f25068a441ca
Schandlich <lukesigler@outlook.com>
2013-08-23 16:43:02 -0700

    Added Missing Files. #418

---
+++ src/ScriptCs.Core/CompilationExceptionWriter.cs
@@ -0,0 +1,16 @@
+﻿using ScriptCs.Contracts;
+using System;
+
+namespace ScriptCs
+{
+    public class CompilationExceptionWriter : ICompilationExceptionWriter
+    {
+        public string CompilationExceptionFilePath
+        {
+            get
+            {
+                return System.Environment.CurrentDirectory + "\\" + "cex_" + DateTime.UtcNow.ToString("YYYYddMM") + ".txt";
+            }
+        }
+    }
+}
