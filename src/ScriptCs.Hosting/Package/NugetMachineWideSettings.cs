8b661d4ebb4708d409070bcd00be3cda168ae6fc
Filip W <wojcieszyn@gmail.com>
2013-08-26 22:32:59 +0200

    upgraded tests projects to use Nuget 2.7 too. scriptcs build version should be alpha

--- src/ScriptCs.Hosting/Package/NugetMachineWideSettings.cs
+++ src/ScriptCs.Hosting/Package/NugetMachineWideSettings.cs
@@ -6,21 +6,19 @@ namespace ScriptCs.Hosting.Package
 {
     internal class NugetMachineWideSettings : IMachineWideSettings
     {
-        //readonly Lazy<IEnumerable<Settings>> _settings;
-        private readonly IEnumerable<Settings> _settings;
+        private readonly Lazy<IEnumerable<Settings>> _settings;
 
         public NugetMachineWideSettings()
         {
             var baseDirectory = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-            //_settings = new Lazy<IEnumerable<NuGet.Settings>>(() => NuGet.Settings.LoadMachineWideSettings(new PhysicalFileSystem(baseDirectory)));
-            _settings = NuGet.Settings.LoadMachineWideSettings(new PhysicalFileSystem(baseDirectory));
+            _settings = new Lazy<IEnumerable<NuGet.Settings>>(() => NuGet.Settings.LoadMachineWideSettings(new PhysicalFileSystem(baseDirectory)));
         }
 
         public IEnumerable<Settings> Settings
         {
             get
             {
-                return _settings;
+                return _settings.Value;
             }
         }
     }
