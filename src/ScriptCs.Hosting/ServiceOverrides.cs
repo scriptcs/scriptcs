cad2ce6324d365e198d4ae78577244a17d030dbf
filipw <wojcieszyn@gmail.com>
2013-11-08 09:18:31 +0100

    Add ISCriptHostFactory to builder/overrides

--- src/ScriptCs.Hosting/ServiceOverrides.cs
+++ src/ScriptCs.Hosting/ServiceOverrides.cs
@@ -10,13 +10,19 @@ namespace ScriptCs
     {
         protected readonly IList<Type> LineProcessors = new List<Type>();
 
-        protected readonly IDictionary<Type, object> Overrides = new Dictionary<Type, object>();
+        public readonly IDictionary<Type, object> Overrides = new Dictionary<Type, object>();
 
         private readonly TConfig _this;
 
         protected ServiceOverrides()
         {
             _this = this as TConfig;
+        }
+
+        public TConfig ScriptHostFactory<T>() where T : IScriptHostFactory
+        {
+            Overrides[typeof (IScriptHostFactory)] = typeof (T);
+            return _this;
         }
 
         protected ServiceOverrides(IDictionary<Type, object> overrides)
