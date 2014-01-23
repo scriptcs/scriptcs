57fd4e73097a4ca680aa597a12b8fa7da50097ec
filipw <wojcieszyn@gmail.com>
2014-01-23 10:16:02 +0100

    added tests

--- test/ScriptCs.Core.Tests/PackageAssemblyResolverTests.cs
+++ test/ScriptCs.Core.Tests/PackageAssemblyResolverTests.cs
@@ -14,6 +14,34 @@ using PackageReference = ScriptCs.Package.PackageReference;
 
 namespace ScriptCs.Tests
 {
+    public class PackageReferenceTests
+    {
+        public class Constructor
+        {
+            [Fact]
+            public void WhenStringVersionIsEmptyVersionShouldBeEmpty()
+            {
+                var p = new PackageReference("packageId", new FrameworkName(".NETFramework,Version=v4.0"), "");
+                p.Version.ShouldEqual(new Version());
+            }
+
+            [Fact]
+            public void WhenStringVersionHasNormalValueVersionShouldBeEqualToThat()
+            {
+                var p = new PackageReference("packageId", new FrameworkName(".NETFramework,Version=v4.0"), "1.0.1");
+                p.Version.ShouldEqual(new Version("1.0.1"));
+            }
+
+            [Fact]
+            public void WhenStringVersionHasSpecialValueVersionShouldBeEqualToNormalAndSpecialVersionShouldBeSet()
+            {
+                var p = new PackageReference("packageId", new FrameworkName(".NETFramework,Version=v4.0"), "1.0.1-alpha");
+                p.Version.ShouldEqual(new Version("1.0.1"));
+                p.SpecialVersion.ShouldEqual("alpha");
+            }
+        }
+    }
+
     public class PackageAssemblyResolverTests
     {
         public class GetAssemblyNamesMethod
