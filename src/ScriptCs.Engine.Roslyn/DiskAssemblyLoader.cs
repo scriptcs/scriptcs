39e021c7afd73f952dd827a02f1935f50d5c754b
dschenkelman <damian.schenkelman@gmail.com>
2014-01-26 14:27:32 -0800

    Removed RoslynScriptPersistentEngine.cs and RoslynScriptInMemoryEngine.cs favoring compositon for RoslynScriptCompilerEngine.cs. Still need to add unit tests.

--- src/ScriptCs.Engine.Roslyn/DiskAssemblyLoader.cs
+++ src/ScriptCs.Engine.Roslyn/DiskAssemblyLoader.cs
@@ -4,49 +4,51 @@ using System.Linq;
 using System.Reflection;
 
 using Common.Logging;
-
 using ScriptCs.Contracts;
 
 namespace ScriptCs.Engine.Roslyn
 {
-    public class RoslynScriptPersistentEngine : RoslynScriptCompilerEngine
+    public class DiskAssemblyLoader : IAssemblyLoader
     {
         private readonly IFileSystem _fileSystem;
         private const string RoslynAssemblyNameCharacter = "ℛ";
+        private readonly ILog _logger;
+        private string _cacheDirectory;
+        private string _scriptFileName;
 
-        public RoslynScriptPersistentEngine(IScriptHostFactory scriptHostFactory, ILog logger, IFileSystem fileSystem)
-            : base(scriptHostFactory, logger)
+        public DiskAssemblyLoader(IScriptHostFactory scriptHostFactory, ILog logger, IFileSystem fileSystem)
         {
             _fileSystem = fileSystem;
+            _logger = logger;
         }
 
-        protected override bool ShouldCompile()
+        public bool ShouldCompile()
         {
             var dllPath = GetDllTargetPath();
 
             return !_fileSystem.FileExists(dllPath);
         }
 
-        protected override Assembly LoadAssembly(byte[] exeBytes, byte[] pdbBytes)
+        public Assembly Load(byte[] exeBytes, byte[] pdbBytes)
         {
-            this.Logger.DebugFormat("Writing assembly to {0}.", FileName);
+            _logger.DebugFormat("Writing assembly to {0}.", _scriptFileName);
 
-            if (!_fileSystem.DirectoryExists(CacheDirectory))
+            if (!_fileSystem.DirectoryExists(_cacheDirectory))
             {
-                _fileSystem.CreateDirectory(CacheDirectory, true);
+                _fileSystem.CreateDirectory(_cacheDirectory, true);
             }
 
             var dllPath = GetDllTargetPath();
             _fileSystem.WriteAllBytes(dllPath, exeBytes);
 
-            Logger.DebugFormat("Loading assembly {0}.", dllPath);
+            _logger.DebugFormat("Loading assembly {0}.", dllPath);
 
             // the assembly is automatically loaded into the AppDomain when compiled
             // just need to find and return it
             return AppDomain.CurrentDomain.GetAssemblies().LastOrDefault(x => x.FullName.StartsWith(RoslynAssemblyNameCharacter));
         }
 
-        protected override Assembly LoadAssemblyFromCache()
+        public Assembly LoadFromCache()
         {
             var dllPath = GetDllTargetPath();
             return Assembly.LoadFrom(dllPath);
@@ -54,9 +56,15 @@ namespace ScriptCs.Engine.Roslyn
 
         private string GetDllTargetPath()
         {
-            var dllName = FileName.Replace(Path.GetExtension(FileName), ".dll");
-            var dllPath = Path.Combine(CacheDirectory, dllName);
+            var dllName = _scriptFileName.Replace(Path.GetExtension(_scriptFileName), ".dll");
+            var dllPath = Path.Combine(_cacheDirectory, dllName);
             return dllPath;
+        }
+
+        public void SetContext(string fileName, string cacheDirectory)
+        {
+            _scriptFileName = fileName;
+            _cacheDirectory = cacheDirectory;
         }
     }
 }
---
+++ src/Scriptcs.Contracts/IScriptLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace ScriptCs.Contracts
+{
+    public interface IAssemblyLoader
+    {
+        void SetContext(string fileName, string cacheDirectory);
+        
+        bool ShouldCompile();
+
+        Assembly Load(byte[] exeBytes, byte[] pdbBytes);
+
+        Assembly LoadFromCache();
+    }
+}
