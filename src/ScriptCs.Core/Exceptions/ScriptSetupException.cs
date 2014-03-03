6d6544dfde407e137d7894b00b6c5b18b18dabb4
filipw <wojcieszyn@gmail.com>
2014-03-03 09:20:48 +0100

    added new Setup Exception

---
+++ src/ScriptCs.Core/Exceptions/ScriptSetupException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ScriptCs.Exceptions
+{
+    [Serializable]
+    public class ScriptSetupException : Exception
+    {
+        public ScriptSetupException(string message)
+            : base(message)
+        {
+        }
+
+        public ScriptSetupException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
