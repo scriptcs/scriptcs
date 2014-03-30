b48205b640e0e75396d9b76094ebfb329c32a09a
Filip W <wojcieszyn@gmail.com>
2014-03-30 20:01:34 +0200

    added InstallCommand

---
+++ src/ScriptCs.Core/InstallCommand.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Linq;
+using System.Runtime.Versioning;
+using Common.Logging;
+using ScriptCs.Contracts;
+
+namespace ScriptCs
+{
+    public class InstallCommand : IReplCommand
+    {
+        private readonly IPackageInstaller _packageInstaller;
+        private readonly IPackageAssemblyResolver _packageAssemblyResolver;
+        private readonly ILog _logger;
+
+        public InstallCommand(IPackageInstaller packageInstaller, IPackageAssemblyResolver packageAssemblyResolver, ILog logger)
+        {
+            _packageInstaller = packageInstaller;
+            _packageAssemblyResolver = packageAssemblyResolver;
+            _logger = logger;
+        }
+
+        public string CommandName
+        {
+            get { return "install"; }
+        }
+
+        public object Execute(IScriptExecutor repl, object[] args)
+        {
+            if (args == null || args.Length == 0) return null;
+
+            string version = null;
+            if (args.Length == 2) version = args[1].ToString();
+
+            _logger.InfoFormat("Installing {0}", args[0]);
+
+            var packageRef = new PackageReference(args[0].ToString(), new FrameworkName(".NETFramework,Version=v4.0"), version);
+            _packageInstaller.InstallPackages(new[]
+            {
+                packageRef
+            });
+            _packageAssemblyResolver.SavePackages();
+
+            var dlls = _packageAssemblyResolver.GetAssemblyNames(repl.FileSystem.CurrentDirectory).Except(repl.References.PathReferences).ToArray();
+            repl.AddReferences(dlls);
+
+            foreach (var dll in dlls)
+            {
+                _logger.InfoFormat("Added reference to {0}", dll);
+            }
+
+            return null;
+        }
+    }
+}
