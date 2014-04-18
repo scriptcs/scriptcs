0dae213c4853a5f58c5ca1ab4fd29b23723ead56
Glenn Block <glenn.block@gmail.com>
2014-04-18 00:53:43 -0700

    Fixed some tests. More to go

--- test/ScriptCs.Tests/CleanCommandTests.cs
+++ test/ScriptCs.Tests/CleanCommandTests.cs
@@ -2,6 +2,7 @@
 using Ploeh.AutoFixture.Xunit;
 using ScriptCs.Command;
 using ScriptCs.Contracts;
+using ScriptCs.Hosting;
 using Xunit.Extensions;
 
 namespace ScriptCs.Tests
@@ -13,14 +14,20 @@ namespace ScriptCs.Tests
             [ScriptCsAutoData(Constants.PackagesFolder)]
             [ScriptCsAutoData(Constants.DllCacheFolder)]
             [Theory]
-            public void ShouldDeletePackagesFolder(string folder, [Frozen] Mock<IFileSystem> fileSystem, CommandFactory factory)
+            public void ShouldDeletePackagesFolder(string folder, 
+                [Frozen] Mock<IFileSystem> fileSystem,
+                [Frozen] Mock<IScriptServicesBuilder> servicesBuilder,
+                [Frozen] Mock<InitializationServices> initializationServices,
+                ScriptServices services,
+                CommandFactory factory)
             {
                 // Arrange
                 var args = new ScriptCsArgs { Clean = true };
 
                 fileSystem.Setup(i => i.DirectoryExists(It.Is<string>(x => x.Contains(Constants.PackagesFolder)))).Returns(true);
                 fileSystem.Setup(i => i.GetWorkingDirectory(It.IsAny<string>())).Returns("c:\\");
-
+                servicesBuilder.Setup(b => b.Build()).Returns(services);
+                initializationServices.Setup(i => i.GetFileSystem()).Returns(fileSystem.Object);
                 // Act
                 factory.CreateCommand(args, new string[0]).Execute();
 
