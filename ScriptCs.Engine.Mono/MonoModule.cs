530a554b3dc5f4aac0aaf42e66b6b20505fdacf5
Glenn Block <glenn.block@gmail.com>
2014-04-22 00:11:07 -0700

    removing logic that forces mono to be no-op if running on .NETFX

--- ScriptCs.Engine.Mono/MonoModule.cs
+++ ScriptCs.Engine.Mono/MonoModule.cs
@@ -11,7 +11,7 @@ namespace ScriptCs.Engine.Mono
         public void Initialize(IModuleConfiguration config)
         {
             Console.WriteLine("Mono Engine initialized!");
-            if (!config.Overrides.ContainsKey(typeof(IScriptEngine)) && Type.GetType("Mono.Runtime") != null)
+            if (!config.Overrides.ContainsKey(typeof(IScriptEngine)))
                 config.ScriptEngine<MonoScriptEngine>();
         }
     }
