82b3e94d7c7edfd93f9c2f1a181a242e9c315f25
Kristian Hellang <kristian@hellang.com>
2014-05-18 23:41:16 +0200

    Added Regex to remove JSON.NET '$ref' and '$id' properties

--- src/ScriptCs.Hosting/ObjectSerializer.cs
+++ src/ScriptCs.Hosting/ObjectSerializer.cs
@@ -1,10 +1,15 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 using ScriptCs.Contracts;
 
 namespace ScriptCs.Hosting
 {
     public class ObjectSerializer : IObjectSerializer
     {
+        private static readonly Regex RefOrIdRegex = new Regex(string.Format(@"(.+\""(\$id|\$ref)\"".+{0})", Environment.NewLine),
+            RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+        
         private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
         {
             PreserveReferencesHandling = PreserveReferencesHandling.Objects,
@@ -14,7 +19,7 @@ namespace ScriptCs.Hosting
 
         public string Serialize(object value)
         {
-            return JsonConvert.SerializeObject(value, Formatting.Indented, Settings);
+            return RefOrIdRegex.Replace(JsonConvert.SerializeObject(value, Formatting.Indented, Settings), string.Empty);
         }
     }
 }
