75a764a4e55cd4bea299ed5acfff4f4d53121e09
Adam Ralph <adam@adamralph.com>
2014-05-31 02:12:39 +0200

    red (mono): added ScriptExecution.ScriptThrowsAnException and switched to runtime Mono conditional

--- test/ScriptCs.Tests.Acceptance/ScriptExecution.cs
+++ test/ScriptCs.Tests.Acceptance/ScriptExecution.cs
@@ -1,9 +1,11 @@
 ﻿namespace ScriptCs.Tests.Acceptance
 {
+    using System;
     using System.Reflection;
     using ScriptCs.Tests.Acceptance.Support;
     using Should;
     using Xbehave;
+    using Xunit;
 
     public static class ScriptExecution
     {
@@ -20,6 +22,24 @@
 
             "Then I see 'Hello world!'"
                 .f(() => output.ShouldContain("Hello world!"));
+        }
+
+        [Scenario]
+        public static void ScriptThrowsAnException(ScriptFile script, Exception ex)
+        {
+            var scenario = MethodBase.GetCurrentMethod().GetFullName();
+
+            "Given a script which throws an exception"
+                .f(() => script = ScriptFile.Create(scenario).WriteLine(@"throw new Exception(""BOOM!"");"));
+
+            "When I execute the script"
+                .f(() => ex = Record.Exception(() => script.Execute()));
+
+            "Then the script fails"
+                .f(() => ex.ShouldNotBeNull());
+
+            "And I see the exception message"
+                .f(() => ex.Message.ShouldContain("BOOM!"));
         }
     }
 }
