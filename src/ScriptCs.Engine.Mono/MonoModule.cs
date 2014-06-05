70a86c03cdd73447b930394d1f1d2268e149a6ce
Adam Ralph <adam@adamralph.com>
2014-06-05 13:46:29 +0200

    refactor: clean up mono and roslyn modules

--- src/ScriptCs.Engine.Mono/MonoModule.cs
+++ src/ScriptCs.Engine.Mono/MonoModule.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using ScriptCs.Contracts;
+﻿using ScriptCs.Contracts;
 
 namespace ScriptCs.Engine.Mono
 {
-    [Module(ModuleName)]
+    [Module("mono")]
     public class MonoModule : IModule
     {
-        public const string ModuleName = "mono";
-
         public void Initialize(IModuleConfiguration config)
         {
             Guard.AgainstNullArgument("config", config);
 
             if (!config.Overrides.ContainsKey(typeof(IScriptEngine)))
+            {
                 config.ScriptEngine<MonoScriptEngine>();
+            }
         }
     }
 }
