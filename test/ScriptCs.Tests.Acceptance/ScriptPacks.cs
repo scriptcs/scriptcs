87f266ca27154fcc2eb48fd237aaa7e35a54ec6a
Adam Ralph <adam@adamralph.com>
2014-06-14 20:11:40 +0200

    green: added basic ScriptPacks acceptance test

---
+++ test/ScriptCs.Tests.Acceptance/ScriptPacks.cs
@@ -0,0 +1,29 @@
+﻿namespace ScriptCs.Tests.Acceptance
+{
+    using System.Reflection;
+    using ScriptCs.Tests.Acceptance.Support;
+    using Should;
+    using Xbehave;
+
+    public static class ScriptPacks
+    {
+        [Scenario]
+        public static void UsingAScriptPack(ScriptFile script, string output)
+        {
+            var scenario = MethodBase.GetCurrentMethod().GetFullName();
+
+            "Given a script which uses ScriptCs.Adder to print the sum of 1234 and 5678"
+                .f(() => script = ScriptFile.Create(scenario, true).WriteLine(
+                    @"Console.WriteLine(Require<Adder>().Add(1234, 5678));"));
+
+            "When I install ScriptCs.Adder"
+                .f(() => script.Install("ScriptCs.Adder.Local"));
+
+            "And execute the script"
+                .f(() => output = script.Execute());
+
+            "Then I see 6912"
+                .f(() => output.ShouldContain("6912"));
+        }
+    }
+}
