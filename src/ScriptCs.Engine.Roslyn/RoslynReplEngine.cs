702c90bd0c79be82a79fc5a371c90317bfacaf18
filipw <wojcieszyn@gmail.com>
2014-09-07 12:14:28 +0200

    retrieve vars using reflection instead of static analysis

--- src/ScriptCs.Engine.Roslyn/RoslynReplEngine.cs
+++ src/ScriptCs.Engine.Roslyn/RoslynReplEngine.cs
@@ -1,10 +1,9 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using Common.Logging;
-using Roslyn.Compilers;
-using Roslyn.Compilers.Common;
 using Roslyn.Scripting;
 using ScriptCs.Contracts;
 
@@ -31,16 +30,23 @@ namespace ScriptCs.Engine.Roslyn
             try
             {
                 var submission = session.CompileSubmission<object>(code);
-                var fields = submission.Compilation.ScriptClass.GetMembers().Where(x => x.Kind == CommonSymbolKind.Field);
-
-                foreach (var field in fields)
-                {
-                    LocalVariables.Add(field.Name);
-                }
 
                 try
                 {
                     var result = submission.Execute();
+
+                    var submissionObjectField = session.GetType().GetField("submissions", BindingFlags.Instance | BindingFlags.NonPublic);
+                    var submissionObject = (submissionObjectField.GetValue(session) as object[]).LastOrDefault(x => x != null);
+
+                    if (submissionObject != null)
+                    {
+                        var fields = submissionObject.GetType().GetFields().Where(x => x.Name.ToLowerInvariant() != "<host-object>");
+                        foreach (var field in fields)
+                        {
+                            LocalVariables.Add(string.Format("{0} {1} = {2}", field.FieldType, field.Name, field.GetValue(submissionObject)));
+                        }
+                    }
+
                     return new ScriptResult(returnValue: result);
                 }
                 catch (Exception ex)
