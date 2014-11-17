a71ff6066316ad05b28bd0acc3320a3a4487c1f9
Adam Ralph <adam@adamralph.com>
2014-11-17 10:32:27 +0100

    refactor: add more cases to PackageReferenceTests and clean up

--- test/ScriptCs.Core.Tests/PackageReferenceTests.cs
+++ test/ScriptCs.Core.Tests/PackageReferenceTests.cs
@@ -7,28 +7,28 @@ namespace ScriptCs.Tests
 {
     public class PackageReferenceTests
     {
-        public class Constructor
+        public class TheConstructor
         {
-            ///<summary>
-            /// Should provide correct version and special version.
-            ///</summary>
             [Theory]
-            [InlineData("", "", "")]
-            [InlineData("1.0.1", "1.0.1", "")]
+            [InlineData(null, null, null)]
+            [InlineData("", null, null)]
+            [InlineData(" ", null, null)]
+            [InlineData("1.0.1", "1.0.1", null)]
             [InlineData("1.0.1-alpha", "1.0.1", "alpha")]
             [InlineData("1.0.1-beta-2", "1.0.1", "beta-2")]
-            public void MustHandleAllSortsOfDifferentVersionsAndSpecialVersions(string version, string expectedVersion, string expectedSpecialVersion)
+            public void ParsesVersionAndSpecialVersion(
+                string version, string expectedVersion, string expectedSpecialVersion)
             {
                 // Arrange, Act
-                var packageReference = new PackageReference("packageId", new FrameworkName(".NETFramework,Version=v4.0"), version);
+                var packageReference = new PackageReference(
+                    "packageId", new FrameworkName(".NETFramework,Version=v4.0"), version);
 
                 // Assert
-                packageReference.Version.ShouldEqual(string.IsNullOrWhiteSpace(expectedVersion) ? new Version() : new Version(expectedVersion));
+                packageReference.Version.ShouldEqual(string.IsNullOrWhiteSpace(expectedVersion)
+                    ? new Version()
+                    : new Version(expectedVersion));
 
-                if (packageReference.SpecialVersion != null)
-                {
-                    packageReference.SpecialVersion.ShouldEqual(expectedSpecialVersion);
-                }
+                packageReference.SpecialVersion.ShouldEqual(expectedSpecialVersion);
             }
         }
     }
