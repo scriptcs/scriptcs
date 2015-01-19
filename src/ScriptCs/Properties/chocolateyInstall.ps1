b3b7519fb97dd83f79eb92c0582db7e0cbbe5cbc
Travis Illig <tillig@paraesthesia.com>
2015-01-19 15:45:50 -0800

    Added upgrade support from old install
    Added a routine to deal with removing the per-user install of scriptcs
    and per-user PATH setting on upgrade.

--- src/ScriptCs/Properties/chocolateyInstall.ps1
+++ src/ScriptCs/Properties/chocolateyInstall.ps1
@@ -18,6 +18,37 @@
         Remove-Item "$tools\..\lib" -Recurse -Force
     }
 
+    # Handle upgrade from previous packages that installed to the %AppData%/scriptcs folders.
+    $oldPaths = @(
+        "$env:APPDATA\scriptcs",
+        "$env:LOCALAPPDATA\scriptcs"
+    )
+
+    $oldPaths | foreach {
+        # Remove the old user-specific scriptcs folders.
+        if (Test-Path $_) {
+            Remove-Item $_ -Recurse -Force
+        }
+
+        # Remove the user-specific path that got added in previous installs.
+        # There's no Uninstall-ChocolateyPath yet so we need to do it manually.
+        # https://github.com/chocolatey/chocolatey/issues/97
+        $envPath = $env:PATH
+        if ($envPath.ToLower().Contains($_.ToLower())) {
+            $userPath = Get-EnvironmentVariable -Name 'Path' -Scope "User"
+            if($userPath) {
+                $actualPath = [System.Collections.ArrayList]($userPath).Split(";")
+                $actualPath.Remove($_)
+                $newPath =  $actualPath -Join ";"
+                Set-EnvironmentVariable -Name 'Path' -Value $newPath -Scope "User"
+            }
+        }
+
+        Write-Host "'$_' has been removed." -ForegroundColor DarkYellow
+    }
+    Update-SessionEnvironment
+    # End upgrade handling.
+
     Write-ChocolateySuccess 'scriptcs'
 } catch {
     Write-ChocolateyFailure 'scriptcs' "$($_.Exception.Message)"
