06d5aff46b7e8e19b47baa7cd1d1aaf1cb6bd183
Travis Illig <tillig@paraesthesia.com>
2015-01-19 08:10:00 -0800

    Chocolatey installation for multiple users
    Fix for issue #874. Switched the installation script to use automatic
    Chocolatey facilities for installing command-line tools. Chocolatey
    handles putting in a shim to scriptcs.exe and adding the path info. No
    need for uninstall when using this mechanism - Chocolatey does it all
    for you.

--- src/ScriptCs/Properties/chocolateyInstall.ps1
+++ src/ScriptCs/Properties/chocolateyInstall.ps1
@@ -1,24 +1,7 @@
-﻿try { 
-    $oldpaths = @(
-        "$env:APPDATA\scriptcs"
-    )
-
-    $oldpaths | foreach {
-        if (Test-Path $_) {
-            Remove-Item $_ -Recurse -Force
-        }
-
-        Write-Host "'$_' has been removed." -ForegroundColor DarkYellow
-    }
-
+﻿try {
     $tools = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
     $nuget = "$env:ChocolateyInstall\ChocolateyInstall\nuget"
-    $binPath = "$env:LOCALAPPDATA\scriptcs"
     $nugetPath = "$tools\nugets"
-
-    New-Item $binPath -ItemType Directory -Force | Out-Null
-
-    Copy-Item "$tools\scriptcs\*" $binPath -Force
 
     Write-Host "Retrieving NuGet dependencies..." -ForegroundColor DarkYellow
 
@@ -28,19 +11,15 @@
 
     $dependencies.GetEnumerator() | %{ &nuget install $_.Name -version $_.Value -o $nugetPath }
 
-    Get-ChildItem $nugetPath -Filter "*.dll" -Recurse | %{ Copy-Item $_.FullName $binPath -Force }
+    Get-ChildItem $nugetPath -Filter "*.dll" -Recurse | %{ Copy-Item $_.FullName $tools -Force }
     Remove-Item $nugetPath -Recurse -Force
-    New-Item "$tools\scriptcs\scriptcs.exe.ignore" -ItemType File -Force | Out-Null
-    
+
     if (Test-Path "$tools\..\lib") {
         Remove-Item "$tools\..\lib" -Recurse -Force
     }
 
-    Install-ChocolateyPath $binPath
-    Write-Host "scriptcs.exe has been installed to $binpath and has been added to your path." -ForegroundColor DarkYellow
-    Write-Host "You may need to open a new console for the new path to take effect. Happy scripting!" -ForegroundColor DarkYellow
     Write-ChocolateySuccess 'scriptcs'
 } catch {
     Write-ChocolateyFailure 'scriptcs' "$($_.Exception.Message)"
-    throw 
+    throw
 }
