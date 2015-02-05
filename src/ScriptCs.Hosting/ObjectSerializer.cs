1e48d7af6290db05566c0378dcd6b7d0463bf96b
Adam Ralph <adam@adamralph.com>
2015-02-05 15:13:48 +0100

    green: changed ObjectSerializer to remove $id and $ref unless there is a circular ref

--- src/ScriptCs.Hosting/ObjectSerializer.cs
+++ src/ScriptCs.Hosting/ObjectSerializer.cs
@@ -1,20 +1,52 @@
-﻿using Newtonsoft.Json;
+﻿using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using ScriptCs.Contracts;
 
 namespace ScriptCs.Hosting
 {
     public class ObjectSerializer : IObjectSerializer
     {
-        private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        private static readonly JsonSerializerSettings settings = new JsonSerializerSettings
         {
+            Formatting = Formatting.Indented,
             PreserveReferencesHandling = PreserveReferencesHandling.Objects,
             ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
-            MaxDepth = 4
         };
 
         public string Serialize(object value)
         {
-            return JsonConvert.SerializeObject(value, Formatting.Indented, Settings);
+            var writer = new JTokenWriter();
+            var serializer = JsonSerializer.Create(settings);
+            serializer.Serialize(writer, value);
+
+            var container = writer.Token as JContainer;
+            if (container != null)
+            {
+                var idProperties = container.Descendants().OfType<JProperty>().Where(d => d.Name == "$id").ToList();
+                if (idProperties.Any())
+                {
+                    var refProperties = container.Descendants().OfType<JProperty>().Where(d => d.Name == "$ref").ToList();
+                    if (refProperties.Any())
+                    {
+                        foreach (var idProperty in idProperties
+                            .Where(idProperty => refProperties
+                                .All(refProperty => refProperty.Value.ToString() != idProperty.Value.ToString())))
+                        {
+                            idProperty.Remove();
+                        }
+                    }
+                    else
+                    {
+                        foreach (var idProperty in idProperties)
+                        {
+                            idProperty.Remove();
+                        }
+                    }
+                }
+            }
+
+            return writer.Token.ToString();
         }
     }
 }
