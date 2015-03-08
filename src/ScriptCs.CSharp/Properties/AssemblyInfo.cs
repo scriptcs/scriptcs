320821af37f9ef8fb4d4a197820900ab75bf5aa5
filipw <wojcieszyn@gmail.com>
2015-03-08 11:53:41 +0100

    updated AssemblyInfo.cs

--- src/ScriptCs.CSharp/Properties/AssemblyInfo.cs
+++ src/ScriptCs.CSharp/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("ScriptCs.Engine.Roslyn")]
-[assembly: AssemblyDescription("ScriptCs.Engine.Roslyn provides a Roslyn-based script engine for scriptcs.")]
+[assembly: AssemblyTitle("ScriptCs.CSharp")]
+[assembly: AssemblyDescription("ScriptCs.CSharp provides a Microsoft.CodeAnalysis-based script engine for scriptcs.")]
 
 // NOTE (adamralph): the assembly versions are fixed at 0.0.0.0 - only NuGet package versions matter
 [assembly: AssemblyVersion("0.0.0.0")]
