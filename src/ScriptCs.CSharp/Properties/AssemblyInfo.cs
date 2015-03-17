b0ef141d866b3efaadc0a83b144fb098a6901ed6
Adam Ralph <adam@adamralph.com>
2015-03-17 10:23:20 +0100

    fix build script after rename and split into 3 projects

--- src/ScriptCs.CSharp/Properties/AssemblyInfo.cs
+++ src/ScriptCs.CSharp/Properties/AssemblyInfo.cs
@@ -1,11 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("ScriptCs.CSharp")]
-[assembly: AssemblyDescription("ScriptCs.CSharp provides a Microsoft.CodeAnalysis-based script engine for scriptcs.")]
-
-// NOTE (adamralph): the assembly versions are fixed at 0.0.0.0 - only NuGet package versions matter
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-
-// NOTE (adamralph): this is used for the NuGet package version
-[assembly: AssemblyInformationalVersion("0.1.1")]
+[assembly: AssemblyDescription(
+    "ScriptCs.CSharp provides a Microsoft.CodeAnalysis-based C# script engine for scriptcs.")]
