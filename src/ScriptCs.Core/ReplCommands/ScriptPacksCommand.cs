5e1113ef5299c44b49a2aa7c5e04b2a0fb63cd0e
filipw <wojcieszyn@gmail.com>
2015-03-27 18:38:24 +0100

    added script packs command outline

---
+++ src/ScriptCs.Core/ReplCommands/ScriptPacksCommand.cs
@@ -0,0 +1,22 @@
+﻿using ScriptCs.Contracts;
+
+namespace ScriptCs.ReplCommands
+{
+    public class ScriptPacksCommand : IReplCommand
+    {
+        public string Description
+        {
+            get { return "Displays information about script packs available in the REPL session"; }
+        }
+
+        public string CommandName
+        {
+            get { return "scriptpacks"; }
+        }
+
+        public object Execute(IRepl repl, object[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
