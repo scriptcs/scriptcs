ef895af5b9742062cb1ac5df93a10d85908a7ca5
filipw <wojcieszyn@gmail.com>
2015-03-28 01:48:04 +0100

    added method dumping, property dumping and extension method support

--- src/ScriptCs.Core/ReplCommands/ScriptPacksCommand.cs
+++ src/ScriptCs.Core/ReplCommands/ScriptPacksCommand.cs
@@ -1,9 +1,22 @@
-﻿using ScriptCs.Contracts;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using ScriptCs.Contracts;
 
 namespace ScriptCs.ReplCommands
 {
     public class ScriptPacksCommand : IReplCommand
     {
+        private readonly IConsole _console;
+
+        public ScriptPacksCommand(IConsole console)
+        {
+            _console = console;
+        }
+
         public string Description
         {
             get { return "Displays information about script packs available in the REPL session"; }
@@ -16,7 +29,144 @@ namespace ScriptCs.ReplCommands
 
         public object Execute(IRepl repl, object[] args)
         {
-            throw new System.NotImplementedException();
+            var packContexts = repl.ScriptPackSession.Contexts;
+
+            if (packContexts == null || !packContexts.Any())
+            {
+                _console.WriteLine("There are no script packs available in this REPL session");
+                return null;
+            }
+
+            var importedNamespaces = repl.Namespaces.Union(repl.ScriptPackSession.Namespaces).ToArray();
+
+            foreach (var packContext in packContexts)
+            {
+                var contextType = packContext.GetType();
+                _console.WriteLine(contextType.ToString());
+
+                var methods = contextType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly).
+                    Where(m => !m.IsSpecialName).Union(GetExtensionMethods(contextType)).OrderBy(x => x.Name);
+                var properties = contextType.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+
+                if (methods.Any())
+                {
+                    _console.WriteLine("** Methods **");
+                    foreach (var method in methods)
+                    {
+                        var methodParams =
+                            method.GetParameters().Select(p => string.Format("{0} {1}", GetPrintableType(p.ParameterType, importedNamespaces), p.Name));
+
+                        if (method.IsDefined(typeof(ExtensionAttribute), false))
+                        {
+                            methodParams = methodParams.Skip(1);
+                        }
+
+                        var signature = string.Format(" - {0} {1}({2})", GetPrintableType(method.ReturnType, importedNamespaces), method.Name,
+                            string.Join(", ", methodParams));
+                        _console.WriteLine(signature);
+                    }
+                    _console.WriteLine();
+                }
+
+                if (properties.Any())
+                {
+                    _console.WriteLine("** Properties **");
+                    foreach (var property in properties)
+                    {
+                        var signature = string.Format(" - {0} {1}", GetPrintableType(property.PropertyType, importedNamespaces), property.Name);
+                        _console.WriteLine(signature);
+                    }
+                }
+
+                _console.WriteLine();
+            }
+
+            return null;
+        }
+
+        private string GetPrintableType(Type type, string[] importedNamespaces)
+        {
+            if (type.Name == "Void")
+            {
+                return "void";
+            }
+
+            if (type.Name == "Object")
+            {
+                return "object";
+            }
+
+            if (type.IsGenericType)
+            {
+                return BuildGeneric(type, importedNamespaces);
+            }
+
+            var nullableType = Nullable.GetUnderlyingType(type);
+            if (nullableType != null)
+            {
+                return string.Format("{0}?", GetPrintableType(nullableType, importedNamespaces));
+            }
+
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Boolean:
+                    return "bool";
+                case TypeCode.Byte:
+                    return "byte";
+                case TypeCode.Char:
+                    return "char";
+                case TypeCode.Decimal:
+                    return "decimal";
+                case TypeCode.Double:
+                    return "double";
+                case TypeCode.Int16:
+                    return "short";
+                case TypeCode.Int32:
+                    return "int";
+                case TypeCode.Int64:
+                    return "long";
+                case TypeCode.SByte:
+                    return "sbyte";
+                case TypeCode.Single:
+                    return "Single";
+                case TypeCode.String:
+                    return "string";
+                case TypeCode.UInt16:
+                    return "UInt16";
+                case TypeCode.UInt32:
+                    return "UInt32";
+                case TypeCode.UInt64:
+                    return "UInt64";
+                default:
+                    return string.IsNullOrEmpty(type.FullName) || importedNamespaces.Contains(type.Namespace)
+                        ? type.Name
+                        : type.FullName;
+            }
+        }
+
+        private string BuildGeneric(Type type, string[] importedNamespaces)
+        {
+            var baseName = type.Name.Substring(0, type.Name.IndexOf("`", StringComparison.Ordinal));
+            var genericDefinition = new StringBuilder(string.Format("{0}<", baseName));
+            var firstArgument = true;
+            foreach (var t in type.GetGenericArguments())
+            {
+                if (!firstArgument)
+                {
+                    genericDefinition.Append(", ");
+                }
+                genericDefinition.Append(GetPrintableType(t, importedNamespaces));
+                firstArgument = false;
+            }
+            genericDefinition.Append(">");
+            return genericDefinition.ToString();
+        }
+
+        private static IEnumerable<MethodInfo> GetExtensionMethods(Type type)
+        {
+            return type.Assembly.GetExportedTypes().Where(x => !x.IsGenericType && !x.IsNested && x.IsSealed).
+                SelectMany(x => x.GetMethods(BindingFlags.Static | BindingFlags.Public)).Where(x => x.IsDefined(typeof(ExtensionAttribute), false)).
+                Where(x => x.GetParameters()[0].ParameterType == type);
         }
     }
 }
