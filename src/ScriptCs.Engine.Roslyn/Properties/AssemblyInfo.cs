1ba5939b94be596a54458bded397751842a4f6ee
filipw <wojcieszyn@gmail.com>
2015-07-26 21:43:28 +0200

    removed unneeded ScriptCs.Engine.Roslyn

--- src/ScriptCs.Engine.Roslyn/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("ScriptCs.Engine.Roslyn")]
-[assembly: AssemblyDescription("ScriptCs.Engine.Roslyn provides a Roslyn-based script engine for scriptcs.")]
