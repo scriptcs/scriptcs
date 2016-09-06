5a16d28ee637141c037a9aa0b92dc1b80a9787b3
Glenn Block <glenn.block@gmail.com>
2016-09-06 00:34:35 -0700

    Fixes #1172 and #1173

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ mono ./.nuget/NuGet.exe restore ./ScriptCs.sln
 # script
 mkdir -p artifacts/Release/bin
 xbuild ./ScriptCs.sln /property:Configuration=Release /nologo /verbosity:normal
-cp src/*/bin/Release/* artifacts/Release/bin/
+cp src/Scriptcs/bin/Release/* artifacts/Release/bin/
 mono ./packages/xunit.runners.1.9.2/tools/xunit.console.clr4.exe test/ScriptCs.Tests.Acceptance/bin/Release/ScriptCs.Tests.Acceptance.dll /xml artifacts/ScriptCs.Tests.Acceptance.dll.TestResult.xml /html artifacts/ScriptCs.Tests.Acceptance.dll.TestResult.html
 
