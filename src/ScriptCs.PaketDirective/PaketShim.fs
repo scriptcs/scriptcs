aa81b3bf892914dce1a8fcf040648e57954ef71d
Gauthier Segay <gauthier.segay@gmail.com>
2017-02-26 02:00:28 +0100

    initialize a PaketDirective project

---
+++ src/ScriptCs.PaketDirective/PaketShim.fs
@@ -0,0 +1,6 @@
+﻿namespace ScriptCs.PaketDirective
+open System
+open System.IO
+module PaketShim =
+    let ResolveLoadScript(scriptFile: FileInfo, packageManagerTextLines: string array, alterToolPath: Func<string,string>) =
+        ReferenceLoading.PaketHandler.Internals.ResolvePackages alterToolPath.Invoke (scriptFile.Directory.FullName, scriptFile.Name, Array.toList packageManagerTextLines)
