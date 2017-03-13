390d9e662395426122635db346580cd18d885bc6
David Nelson <eatdrinksleepcode@gmail.com>
2017-03-13 01:45:48 -0500

    Use Build.proj for version determination and build

--- build.sh
+++ build.sh
@@ -5,13 +5,12 @@ set -x
 
 # install
 mozroots --import --sync --quiet
-mono ./.nuget/NuGet.exe restore ./ScriptCs.sln
 
 # script
 mkdir -p artifacts/Release/bin
-xbuild ./build/Build.proj /property:Configuration=Release /nologo /v:n /target:UpdateProjectVersions
-xbuild ./ScriptCs.sln /property:Configuration=Release /nologo /v:n /fl "/flp:LogFile=artifacts/msbuild.log;Verbosity=diagnostic;DetailedSummary"
-xbuild ./build/Build.proj /property:Configuration=Release /nologo /v:n /target:RestoreCommonVersionInfo
+xbuild ./build/Build.proj /nologo /v:M /t:Build "$@" /fl "/flp:LogFile=artifacts/msbuild.log;Verbosity=diagnostic;DetailedSummary"
 
-cp src/*/bin/Release/* artifacts/Release/bin/ 2>/dev/null
+# Not using Build.proj to copy artifacts or run tests due to bug: wildcard includes have incorrect and duplicate files
+# https://bugzilla.xamarin.com/show_bug.cgi?id=31628
+cp -r src/*/bin/Release artifacts/Release/bin/
 mono ./packages/xunit.runners.1.9.2/tools/xunit.console.clr4.exe test/ScriptCs.Tests.Acceptance/bin/Release/ScriptCs.Tests.Acceptance.dll /xml artifacts/ScriptCs.Tests.Acceptance.dll.TestResult.xml /html artifacts/ScriptCs.Tests.Acceptance.dll.TestResult.html
