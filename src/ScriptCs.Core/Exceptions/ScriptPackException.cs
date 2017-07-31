86e76ad3292486b40afd0f44691e99ee530a7845
filipw <wojcieszyn@gmail.com>
2017-07-31 13:29:32 +0200

    print a better exception when script pack is not found

---
+++ src/ScriptCs.Core/Exceptions/ScriptPackException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ScriptCs.Exceptions
+{
+    [Serializable]
+    public class ScriptPackException : Exception
+    {
+        public ScriptPackException(string message)
+            : base(message)
+        {
+        }
+
+        protected ScriptPackException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
