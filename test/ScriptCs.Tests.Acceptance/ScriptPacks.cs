5a47d86b228245d914a8994a40b269b78ff63a60
filipw <wojcieszyn@gmail.com>
2017-07-31 13:29:57 +0200

    acceptance test for trying to resolve a missing script pack

--- test/ScriptCs.Tests.Acceptance/ScriptPacks.cs
+++ test/ScriptCs.Tests.Acceptance/ScriptPacks.cs
@@ -4,6 +4,9 @@
     using ScriptCs.Tests.Acceptance.Support;
     using Should;
     using Xbehave;
+    using System;
+    using Should.Core.Assertions;
+    using ScriptCs.Exceptions;
 
     public static class ScriptPacks
     {
@@ -24,6 +27,30 @@
 
             "Then I see 6912"
                 .f(() => output.ShouldContain("6912"));
+        }
+
+        [Scenario]
+        public static void UsingAnUnavailableScriptPack(ScenarioDirectory directory, string output, Exception exception)
+        {
+            var scenario = MethodBase.GetCurrentMethod().GetFullName();
+
+            "Given a script which uses a non-existing script pack 'Foo'"
+                .f(() => directory = ScenarioDirectory.Create(scenario)
+                    .WriteLine("foo.csx", @"var fooContext = Require<Foo>();
+                        class Foo : ScriptCs.Contracts.IScriptPackContext{}
+                        Console.WriteLine(""hi"");"));
+
+            "When I execute the script"
+                .f(() => exception = Record.Exception(() => ScriptCsExe.Run("foo.csx", directory)));
+
+            "Then scriptcs fails"
+                .f(() => exception.ShouldBeType<ScriptCsException>());
+
+            "With a script pack exception"
+                .f(() =>
+                {
+                    exception.Message.ShouldContain("Tried to resolve a script pack 'Submission#0+Foo', but such script pack is not available in the current execution context.");
+                });
         }
     }
 }
