e2dce126c0c350b1701866196028907c18d6b6c8
Filip W <wojcieszyn@gmail.com>
2017-08-01 18:51:38 +0200

    f() to x()

--- test/ScriptCs.Tests.Acceptance/ScriptPacks.cs
+++ test/ScriptCs.Tests.Acceptance/ScriptPacks.cs
@@ -16,17 +16,17 @@
             var scenario = MethodBase.GetCurrentMethod().GetFullName();
 
             "Given a script which uses ScriptCs.Adder to print the sum of 1234 and 5678"
-                .f(() => directory = ScenarioDirectory.Create(scenario)
+                .x(() => directory = ScenarioDirectory.Create(scenario)
                     .WriteLine("foo.csx", @"Console.WriteLine(Require<Adder>().Add(1234, 5678));"));
 
             "And ScriptCs.Adder is installed"
-                .f(() => ScriptCsExe.Install("ScriptCs.Adder.Local", directory));
+                .x(() => ScriptCsExe.Install("ScriptCs.Adder.Local", directory));
 
             "When execute the script"
-                .f(() => output = ScriptCsExe.Run("foo.csx", directory));
+                .x(() => output = ScriptCsExe.Run("foo.csx", directory));
 
             "Then I see 6912"
-                .f(() => output.ShouldContain("6912"));
+                .x(() => output.ShouldContain("6912"));
         }
 
         [Scenario]
@@ -35,19 +35,19 @@
             var scenario = MethodBase.GetCurrentMethod().GetFullName();
 
             "Given a script which uses a non-existing script pack 'Foo'"
-                .f(() => directory = ScenarioDirectory.Create(scenario)
+                .x(() => directory = ScenarioDirectory.Create(scenario)
                     .WriteLine("foo.csx", @"var fooContext = Require<Foo>();
                         class Foo : ScriptCs.Contracts.IScriptPackContext{}
                         Console.WriteLine(""hi"");"));
 
             "When I execute the script"
-                .f(() => exception = Record.Exception(() => ScriptCsExe.Run("foo.csx", directory)));
+                .x(() => exception = Record.Exception(() => ScriptCsExe.Run("foo.csx", directory)));
 
             "Then scriptcs fails"
-                .f(() => exception.ShouldBeType<ScriptCsException>());
+                .x(() => exception.ShouldBeType<ScriptCsException>());
 
             "With a script pack exception"
-                .f(() =>
+                .x(() =>
                 {
                     exception.Message.ShouldContain("Tried to resolve a script pack 'Submission#0+Foo', but such script pack is not available in the current execution context.");
                 });
