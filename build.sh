145ee9dd23fabbd40930106806bbba3b47363e44
Filip W <wojcieszyn@gmail.com>
2017-08-01 16:31:07 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,5 +12,5 @@ mono ./.nuget/NuGet.exe restore ./ScriptCs.sln
 mkdir -p artifacts/Release/bin
 msbuild ./ScriptCs.sln /property:Configuration=Release /nologo /verbosity:normal
 cp src/ScriptCs/bin/Release/* artifacts/Release/bin/
-mono ./packages/xunit.runner.console.2.2.0/tools/xunit.console.exe test/ScriptCs.Tests.Acceptance/bin/Release/ScriptCs.Tests.Acceptance.dll /xml artifacts/ScriptCs.Tests.Acceptance.dll.TestResult.xml /html artifacts/ScriptCs.Tests.Acceptance.dll.TestResult.html
+mono ./packages/xunit.runner.console.2.2.0/tools/xunit.console.exe test/ScriptCs.Tests.Acceptance/bin/Release/ScriptCs.Tests.Acceptance.dll
 
