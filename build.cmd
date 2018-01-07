30f48a6f97495fe0e319975dbd55604ed2f27130
BlackFrog1 <blackfrog1@gmail.com>
2018-01-06 23:14:23 -0500

    use vswhere to locate msbuild
    Update the build.cmd to locate MSBuild by using the Visual Studio
    Locator

--- build.cmd
+++ build.cmd
@@ -1,22 +1,66 @@
 @if not defined _echo echo off
-setlocal enabledelayedexpansion
+setlocal EnableExtensions EnableDelayedExpansion
 
-@REM Determine if MSBuild is already in the PATH
-for /f "usebackq delims=" %%I in (`where msbuild.exe 2^>nul`) do (
-    goto Start
+@echo Start pre-build setup
+@echo    Restoring global NuGet packages...
+@echo.
+.nuget\NuGet.exe restore .nuget\packages.config -ConfigFile .nuget\NuGet.Config 
+echo.
+
+REM Find the Visual Studio Locator utility
+REM https://github.com/Microsoft/vswhere
+
+for /f "tokens=*" %%a in ('dir packages\vswhere.exe /b /s') do set p=%%a
+if not defined p (
+   @echo Could not locate the Visual Studio Locator
+   goto End
+) 
+
+
+REM Visual Studio Version
+REM Legacy will find all version
+for /f "tokens=1* usebackq delims=" %%v in (`%p% -legacy -version "[14.0,16.0)" -property installationVersion`) do (
+   set VSVersion=%%v
+   REM Only get the first item
+   goto PointA
 )
 
-echo.
-echo MSBuild is not found in your PATH
-echo Run build.cmd from Visual Studio Command Prompt or Developer Command Prompt
-goto end
+:PointA
+
+REM Now find the installation directory
+REM https://support.microsoft.com/en-us/help/2524009/error-running-command-shell-scripts-that-include-parentheses
+
+REM MSBuild is not in VS2015 folder
+REM So we set location to search for it  
+set InstallDir=%PROGRAMFILES(X86)%\msbuild
+
+if not "%VSVersion%" equ "14.0" (
+   REM Visual Studio Installation Directory
+   for /f "usebackq delims=" %%i in (`%p% -latest -version "[15.0,16.0)" -requires Microsoft.Component.MSBuild -property InstallationPath`) do (
+      set InstallDir=%%i
+   )
+)
+
+REM Now locate the MSBuild.exe by searching for it
+set MSBuildSearch="%InstallDir%\msbuild.exe"
+for /f "tokens=*" %%b in ('dir %MSBuildSearch% /b /s') do (
+   set MSBuildExe=%%b
+   REM only want the first one
+   goto PointB
+)
+:PointB
+if not defined MSBuildExe (
+   @echo Could not locate MSBuild.exe
+   goto End
+) 
 
 :Start
 if exist artifacts goto Build
 mkdir artifacts
 
 :Build
-msbuild Build\Build.proj /nologo /m /v:M %* /fl /flp:LogFile=artifacts\msbuild.log;Verbosity=Diagnostic;DetailedSummary /nr:false 
+@echo Starting msbuild...
+"%MSBuildExe%" Build\Build.proj /nologo /m /v:M %* /fl /flp:LogFile=artifacts\msbuild.log;Verbosity=Diagnostic;DetailedSummary /nr:false 
 
 if %ERRORLEVEL% neq 0 goto BuildFail
 goto BuildSuccess
