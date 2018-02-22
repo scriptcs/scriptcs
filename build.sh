e706b1466d096c0e6119dd9257db7aa9fde1cdec
filipw <wojcieszyn@gmail.com>
2018-02-22 11:00:18 +0100

    corrected build files

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ set -x
 
 # install
 mozroots --import --sync --quiet
-dotnet restore
+mono ./.nuget/NuGet.exe restore ./.nuget/packages.config -PackagesDirectory ./packages
+mono ./.nuget/NuGet.exe restore ./ScriptCs.sln
 
 # script
 mkdir -p artifacts/Release/bin
 msbuild ./ScriptCs.sln /property:Configuration=Release /nologo /verbosity:normal
 cp src/ScriptCs/bin/Release/* artifacts/Release/bin/
-mono ./packages/xunit.runner.console.2.3.1/tools/net461/xunit.console.exe test/ScriptCs.Tests.Acceptance/bin/Release/ScriptCs.Tests.Acceptance.dll
-
+mono ./packages/xunit.runner.console.2.3.1/tools/net452/xunit.console.exe test/ScriptCs.Tests.Acceptance/bin/Release/ScriptCs.Tests.Acceptance.dll
