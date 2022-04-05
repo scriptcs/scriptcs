aae4cc49bf0611dfeedf02d70a2232aba7df3d73
Salim Khan <88341550+Salim-Khan-Techsmith@users.noreply.github.com>
2022-04-05 11:56:58 -0400

    Remove Depreacted helper functions

--- src/ScriptCs/Properties/chocolateyInstall.ps1
+++ src/ScriptCs/Properties/chocolateyInstall.ps1
@@ -1,43 +1,36 @@
-﻿try {
-    $tools = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$tools = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-    if (Test-Path "$tools\..\lib") {
-        Remove-Item "$tools\..\lib" -Recurse -Force
-    }
-
-    # Handle upgrade from previous packages that installed to the %AppData%/scriptcs folders.
-    $oldPaths = @(
-        "$env:APPDATA\scriptcs",
-        "$env:LOCALAPPDATA\scriptcs"
-    )
-
-    $oldPaths | foreach {
-        # Remove the old user-specific scriptcs folders.
-        if (Test-Path $_) {
-            Remove-Item $_ -Recurse -Force
-        }
-
-        # Remove the user-specific path that got added in previous installs.
-        # There's no Uninstall-ChocolateyPath yet so we need to do it manually.
-        # https://github.com/chocolatey/chocolatey/issues/97
-        $envPath = $env:PATH
-        if ($envPath.ToLower().Contains($_.ToLower())) {
-            $userPath = [Environment]::GetEnvironmentVariable("Path","User")
-            if($userPath) {
-                $actualPath = [System.Collections.ArrayList]($userPath).Split(";")
-                $actualPath.Remove($_)
-                $newPath =  $actualPath -Join ";"
-                [Environment]::SetEnvironmentVariable("Path", $newPath, "User")
-            }
-        }
-
-        Write-Host "'$_' has been removed." -ForegroundColor DarkYellow
-    }
-    Update-SessionEnvironment
-    # End upgrade handling.
-
-    Write-ChocolateySuccess 'scriptcs'
-} catch {
-    Write-ChocolateyFailure 'scriptcs' "$($_.Exception.Message)"
-    throw
+if (Test-Path "$tools\..\lib") {
+    Remove-Item "$tools\..\lib" -Recurse -Force
 }
+
+# Handle upgrade from previous packages that installed to the %AppData%/scriptcs folders.
+$oldPaths = @(
+    "$env:APPDATA\scriptcs",
+    "$env:LOCALAPPDATA\scriptcs"
+)
+
+$oldPaths | foreach {
+    # Remove the old user-specific scriptcs folders.
+    if (Test-Path $_) {
+        Remove-Item $_ -Recurse -Force
+    }
+
+    # Remove the user-specific path that got added in previous installs.
+    # There's no Uninstall-ChocolateyPath yet so we need to do it manually.
+    # https://github.com/chocolatey/chocolatey/issues/97
+    $envPath = $env:PATH
+    if ($envPath.ToLower().Contains($_.ToLower())) {
+        $userPath = [Environment]::GetEnvironmentVariable("Path","User")
+        if($userPath) {
+            $actualPath = [System.Collections.ArrayList]($userPath).Split(";")
+            $actualPath.Remove($_)
+            $newPath =  $actualPath -Join ";"
+            [Environment]::SetEnvironmentVariable("Path", $newPath, "User")
+        }
+    }
+
+    Write-Host "'$_' has been removed." -ForegroundColor DarkYellow
+}
+Update-SessionEnvironment
+# End upgrade handling.
